e9f4ff761db6da76210f0fe2fc507651afe548ed
leejm1229 <ljm5524@gmail.com>
2024-04-09 00:36:37 +0900

    신규 아이디 추천 성공

--- CodingTest/프로그래머스/KAKAO/Lv1/신규아이디추천.java
+++ CodingTest/프로그래머스/KAKAO/Lv1/신규아이디추천.java
@@ -1,5 +1,26 @@
 package 프로그래머스.KAKAO.Lv1;
 
 public class 신규아이디추천 {
-    
+    public String solution(String new_id) {
+        String answer = "";
+        
+        String recoID = new_id.toLowerCase();
+        recoID = recoID.replaceAll("[^a-z0-9_.-]","");
+        recoID = recoID.replaceAll("\\.{2,}",".");
+        recoID = recoID.replaceAll("^\\.|\\.$","");
+        if (recoID.length()==0) {
+            recoID = "a";
+        } else if (recoID.length()>15) {
+            recoID = recoID.substring(0,15);
+        }
+        recoID = recoID.replaceAll("\\.$","");
+        if (recoID.length()<3) {
+            String last = recoID.substring(recoID.length()-1);
+            while (recoID.length()<3) {
+                recoID += last;
+            }
+        }
+        answer = recoID;
+        return answer;
+    }
 }
