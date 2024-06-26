f2ff1902ad05c8c375d70c13056187f13a8dd245
leejm1229 <ljm5524@gmail.com>
2024-06-26 22:34:45 +0900

    할인정책 추가

--- SpringBoot/Spring_Basic/core/src/main/java/hello/core/member/MemberService.java
+++ SpringBoot/Spring_Basic/core/src/main/java/hello/core/member/MemberService.java
@@ -1,2 +1,6 @@
-package hello.core.member;public interface MemberService {
+package hello.core.member;
+
+public interface MemberService {
+    void join(Member member);
+    Member findMember(Long MemberId);
 }
