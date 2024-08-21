cbf2aa318466166012d0f68ed08a9884f5823f42
leejm1229 <ljm5524@gmail.com>
2024-08-21 21:17:35 +0900

    기초연습

---
+++ SpringBoot/Spring_Basic/coreTest/src/test/java/hello/coreTest/CoreTestApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.coreTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
